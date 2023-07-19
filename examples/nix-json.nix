b6b87efd09d8bdb105b4de36b1eeb2018bebf61a
sempruijs <sem@pruijs.net>
2023-07-19 19:45:18 +0200

    save progress

---
+++ examples/nix-json.nix
@@ -0,0 +1,17 @@
+{
+  name = "sem";
+  numbers = [
+    1
+    2
+    3
+    4
+  ];
+  small-pi = 3.14159265;
+  age = 18;
+
+  atribute-set = {
+    a = "waf!";
+    b = "miauw";
+    c = true;
+  };
+}
---
+++ examples/rec.nix
@@ -0,0 +1,7 @@
+# rec stands for recursive atribute set
+
+rec {
+  a = 1;
+  b = a + 2;
+  c = b + 3;
+}
